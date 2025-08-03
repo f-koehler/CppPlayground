0b661a58a16738539d0eb9c3e5f0ca7fbb38a3f6
Fabian Koehler <fabian.koehler@proton.me>
2025-08-04 01:40:00 +0800

    feat(thread_racer): add a program to race multiple threads and practice concurrent programming

---
+++ src/thread_racer.cpp
@@ -0,0 +1,79 @@
+#include <CLI/CLI.hpp>
+#include <algorithm>
+#include <atomic>
+#include <chrono>
+#include <cmath>
+#include <condition_variable>
+#include <cstdint>
+#include <future>
+#include <iterator>
+#include <memory>
+#include <ranges>
+#include <spdlog/spdlog.h>
+#include <thread>
+#include <vector>
+
+struct Result {
+  uint64_t thread_id;
+  uint64_t time;
+};
+
+int main(int argc, char **argv) {
+  CLI::App app{"Which thread will be the fastest"};
+  argv = app.ensure_utf8(argv);
+
+  std::uint64_t num_threads = 4;
+  app.add_option("-n,--num-threads", num_threads, "Number of threads to race");
+  CLI11_PARSE(app, argc, argv);
+
+  std::atomic_uint64_t num_threads_ready{0};
+  std::condition_variable cv_start;
+  std::mutex mutex;
+
+  auto thread_func = [num_threads, &num_threads_ready, &cv_start, &mutex](
+                         uint64_t thread_id, std::promise<Result> &&promise) {
+    const bool ready_to_go = (++num_threads_ready) == num_threads;
+    if (ready_to_go) {
+      cv_start.notify_all();
+    } else {
+      std::unique_lock<std::mutex> lock(mutex);
+      cv_start.wait(lock, [&num_threads_ready, &num_threads] {
+        return num_threads_ready.load() == num_threads;
+      });
+    }
+
+    volatile uint64_t counter = 0;
+    const auto start = std::chrono::high_resolution_clock::now();
+    while (counter < (uint64_t{1} << 34)) {
+      counter = counter + 1;
+    }
+    const auto stop = std::chrono::high_resolution_clock::now();
+    promise.set_value(Result{
+        thread_id,
+        static_cast<uint64_t>(std::chrono::nanoseconds(stop - start).count())});
+  };
+
+  std::vector<std::jthread> threads;
+  std::vector<std::future<Result>> future_results;
+  threads.reserve(num_threads);
+  future_results.reserve(num_threads);
+  for (uint64_t i = 0; i < num_threads; ++i) {
+    std::promise<Result> promise;
+    future_results.push_back(promise.get_future());
+    threads.emplace_back(thread_func, i, std::move(promise));
+  }
+
+  std::vector<Result> results(num_threads);
+  std::transform(future_results.begin(), future_results.end(), results.begin(),
+                 [](std::future<Result> &future) { return future.get(); });
+
+  std::ranges::sort(results, [](const Result &a, const Result &b) {
+    return a.time < b.time;
+  });
+
+  for (const auto &result : results) {
+    spdlog::info("thread {}: {} ns", result.thread_id, result.time);
+  }
+
+  return 0;
+}
