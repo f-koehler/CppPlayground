b288a2360fd19f6fc500dd7e20564dc3512ff9ff
Fabian Koehler <fabian.koehler@proton.me>
2025-08-14 01:20:56 +0800

    feat(ArcPointer): continue working on ArcPointer

--- include/cpp_playground/arc_pointer.hpp
+++ include/cpp_playground/arc_pointer.hpp
@@ -3,19 +3,52 @@
 
 #include <atomic>
 #include <cstddef>
+#include <memory>
 #include <type_traits>
 
 namespace CppPlayground {
+
+namespace Detail {
+struct ControlBlock {
+  void *m_ptr;
+  // store allocator and deleter with erase type
+  void *(*m_allocator)(std::size_t n);
+  void (*m_deleter)(void *);
+  std::atomic_uint64_t m_num_shared;
+  std::atomic_uint64_t m_num_weak;
+
+  ControlBlock(void *ptr, void *(*allocator)(std::size_t n),
+               void (*deleter)(void *))
+      : m_ptr(ptr), m_allocator(allocator), m_deleter(deleter), m_num_shared{1},
+        m_num_weak{0} {}
+
+  void add_ref() { ++m_num_shared; }
+  void add_weak_ref() { ++m_num_weak; }
+  void release_ref() {
+    if ((--m_num_shared) == 0) {
+      m_deleter(m_ptr);
+      delete this;
+    }
+  }
+};
+}; // namespace Detail
+
 template <typename T> class ArcPointer {
 public:
-  using Type = T;
+  using ElementType = std::remove_extent_t<T>;
 
 private:
-  std::atomic_uint64_t m_reference_count;
-  Type *m_data;
+  Detail::ControlBlock *m_control_block;
+  ElementType *m_ptr;
 
 public:
-  constexpr ArcPointer() noexcept;
+  constexpr ArcPointer() noexcept
+      : m_control_block(new Detail::ControlBlock(
+            nullptr,
+            [](const std::size_t n) {
+              return (void *)std::allocator<ElementType>{}.allocate(n);
+            },
+            [](void *ptr) { delete (ElementType *)ptr; })) {}
   constexpr ArcPointer(std::nullptr_t) noexcept;
 };
 } // namespace CppPlayground
