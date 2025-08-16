9e2070b8f04d94e664ff0548624511afff7822a3
Fabian Koehler <fabian.koehler@proton.me>
2025-08-16 16:24:50 +0800

    feat(arc_pointer): work on arc pointer implementation and testing

--- include/cpp_playground/arc_pointer.hpp
+++ include/cpp_playground/arc_pointer.hpp
@@ -8,47 +8,119 @@
 
 namespace CppPlayground {
 
+template <typename T> struct DefaultDelete {
+  void operator()(void *ptr) { delete (T *)ptr; }
+};
+template <typename T> struct DefaultDeleteArray {
+  void operator()(void *ptr) { delete[] (T *)ptr; }
+};
+
 namespace Detail {
-struct ControlBlock {
+
+struct ControlBlockBase {
   void *m_ptr;
-  // store allocator and deleter with erase type
-  void *(*m_allocator)(std::size_t n);
+  // store deleter with erased type
   void (*m_deleter)(void *);
   std::atomic_uint64_t m_num_shared;
-  std::atomic_uint64_t m_num_weak;
 
-  ControlBlock(void *ptr, void *(*allocator)(std::size_t n),
-               void (*deleter)(void *))
-      : m_ptr(ptr), m_allocator(allocator), m_deleter(deleter), m_num_shared{1},
-        m_num_weak{0} {}
+  explicit ControlBlockBase(void *ptr) : m_ptr(ptr), m_num_shared{1} {}
+  virtual ~ControlBlockBase() = default;
 
+  virtual void destroy_object() = 0;
   void add_ref() { ++m_num_shared; }
-  void add_weak_ref() { ++m_num_weak; }
   void release_ref() {
     if ((--m_num_shared) == 0) {
-      m_deleter(m_ptr);
+      if (m_ptr != nullptr) {
+        destroy_object();
+      }
+      delete this;
     }
   }
 };
-}; // namespace Detail
+
+template <typename Deleter> struct ControlBlock : public ControlBlockBase {
+  // store deleter with EBO
+  [[no_unique_address]] Deleter m_deleter;
+
+  explicit ControlBlock(void *ptr, const Deleter &deleter)
+      : ControlBlockBase(ptr), m_deleter(deleter) {}
+  explicit ControlBlock(void *ptr, Deleter &&deleter)
+      : ControlBlockBase(ptr), m_deleter(deleter) {}
+
+  void destroy_object() override { m_deleter(m_ptr); }
+};
+
+} // namespace Detail
 
 template <typename T> class ArcPointer {
 public:
   using ElementType = std::remove_extent_t<T>;
 
 private:
-  Detail::ControlBlock *m_control_block;
+  Detail::ControlBlockBase *m_control_block;
   ElementType *m_ptr;
 
 public:
-  constexpr ArcPointer() noexcept
-      : m_control_block(new Detail::ControlBlock(
-            nullptr,
-            [](const std::size_t n) {
-              return (void *)std::allocator<ElementType>{}.allocate(n);
-            },
-            [](void *ptr) { delete (ElementType *)ptr; })) {}
-  constexpr ArcPointer(std::nullptr_t) noexcept;
+  // default constructor
+  template <typename Deleter = DefaultDelete<T>>
+  explicit constexpr ArcPointer() noexcept
+      : m_control_block(
+            new Detail::ControlBlock<Deleter>(nullptr, DefaultDelete<T>{})),
+        m_ptr(nullptr) {}
+
+  template <typename Deleter = DefaultDelete<T>>
+  explicit constexpr ArcPointer(ElementType *ptr,
+                                Deleter deleter = Deleter{}) noexcept
+      : m_control_block(
+            new Detail::ControlBlock<Deleter>((void *)ptr, deleter)),
+        m_ptr(ptr) {}
+
+  // copy constructor
+  ArcPointer(const ArcPointer &other)
+      : m_control_block(other.m_control_block), m_ptr(other.m_ptr) {
+    m_control_block->add_ref();
+  }
+
+  // move constructor
+  ArcPointer(ArcPointer &&other)
+      : m_control_block(other.m_control_block), m_ptr(other.m_ptr) {
+    // no benefit of moving our small pointer types, other will be deconstructed
+    // -> decrements ref count
+  }
+
+  // copy assignment
+  ArcPointer &operator=(const ArcPointer &other) {
+    // do nothing if pointing to the same control block
+    if (other.m_control_block == m_control_block) {
+      return *this;
+    }
+
+    other.m_control_block->add_ref();
+    m_control_block->release_ref();
+    m_control_block = other.m_control_block;
+    m_ptr = other.m_ptr;
+    return *this;
+  }
+
+  // move assignment
+  ArcPointer &operator=(ArcPointer &&other) {
+    if (m_control_block == other.m_control_block) {
+      // pointing to the same control block, other will be deconstructed ->
+      // decrements ref count
+      return *this;
+    }
+
+    m_control_block = other.m_control_block;
+    m_ptr = other.m_ptr;
+  }
+
+  virtual ~ArcPointer() {
+    if (m_control_block != nullptr) {
+      m_control_block->release_ref();
+    }
+  }
+
+  ElementType *get() const noexcept { return m_ptr; }
 };
 } // namespace CppPlayground
 
--- tests/test_arc_pointer.cpp
+++ tests/test_arc_pointer.cpp
@@ -1,5 +1,46 @@
+#include <atomic>
 #include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <cpp_playground/arc_pointer.hpp>
+#include <string>
 
-TEST_CASE("ArcPointer", "") { CppPlayground::ArcPointer<int> pointer; }
+using CppPlayground::ArcPointer;
+
+TEMPLATE_TEST_CASE("ArcPointer: Constructors", "[SmartPointer]", int, char,
+                   std::string) {
+  using T = TestType;
+
+  SECTION("Default Constructor") {
+    ArcPointer<T> pointer;
+    REQUIRE(pointer.get() == nullptr);
+  }
+
+  SECTION("From Raw Pointer") {
+    T *raw_pointer = new T{};
+    ArcPointer<T> pointer(raw_pointer);
+    REQUIRE(pointer.get() == raw_pointer);
+  }
+
+  SECTION("From Raw Pointer (Check Dtor)") {
+    // struct {
+    //   std::size_t count = 0;
+    //
+    //   void operator()(void *ptr) {
+    //     delete (T *)ptr;
+    //     ++count;
+    //   }
+    // } deleter;
+
+    T *raw_pointer = new T{};
+    std::size_t count = 0;
+
+    {
+      ArcPointer<T> pointer(raw_pointer, [&count](void *ptr) {
+        delete (T *)ptr;
+        ++count;
+      });
+      REQUIRE(pointer.get() == raw_pointer);
+    }
+    REQUIRE(count == 1);
+  }
+}
