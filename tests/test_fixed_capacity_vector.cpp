087a54fb2e227e0a1e4c52147dd62ea9e512708a
Fabian Koehler <fabian.koehler@proton.me>
2025-11-13 23:10:49 +0800

    feat(FixedCapacityVector): add move constructor with different capacity

--- tests/test_fixed_capacity_vector.cpp
+++ tests/test_fixed_capacity_vector.cpp
@@ -16,13 +16,29 @@ TEST_CASE("FixedCapacityVector: Constructors") {
   }
 
   SECTION("Initializer List Constructor") {
-    FixedCapacityVector<uint64_t, Capacity> vector = {1UL, 2UL, 3UL};
-    REQUIRE(vector.size() == 3UL);
-    REQUIRE(vector.capacity() == Capacity);
-    REQUIRE(!vector.is_empty());
-    REQUIRE(vector.is_full());
-    REQUIRE(vector.front() == 1UL);
-    REQUIRE(vector.back() == 3UL);
+    SECTION("Nominal") {
+      FixedCapacityVector<uint64_t, Capacity> vector = {1UL, 2UL, 3UL};
+      REQUIRE(vector.size() == 3UL);
+      REQUIRE(vector.capacity() == Capacity);
+      REQUIRE(!vector.is_empty());
+      REQUIRE(vector.is_full());
+      REQUIRE(vector.front() == 1UL);
+      REQUIRE(vector.back() == 3UL);
+    }
+
+    SECTION("Input too large") {
+      REQUIRE_THROWS_AS(
+          (FixedCapacityVector<uint64_t, Capacity>{1UL, 2UL, 3UL, 4UL}),
+          std::length_error);
+    }
+
+    SECTION("Empty list") {
+      FixedCapacityVector<uint64_t, Capacity> vector = {};
+      REQUIRE(vector.size() == 0UL);
+      REQUIRE(vector.capacity() == Capacity);
+      REQUIRE(vector.is_empty());
+      REQUIRE(!vector.is_full());
+    }
   }
 
   SECTION("Copy Constructor") {
@@ -66,5 +82,31 @@ TEST_CASE("FixedCapacityVector: Constructors") {
     REQUIRE(moved.is_full());
     REQUIRE(moved.front() == 1UL);
     REQUIRE(moved.back() == 3UL);
+
+    REQUIRE(vector.is_empty());
+  }
+
+  SECTION("Move Constructor (different capacity)") {
+    constexpr uint64_t OtherCapacity = Capacity + 1;
+    SECTION("Input too large") {
+      FixedCapacityVector<uint64_t, OtherCapacity> vector = {1UL, 2UL, 3UL,
+                                                             4UL};
+      REQUIRE_THROWS_AS(
+          (FixedCapacityVector<uint64_t, Capacity>(std::move(vector))),
+          std::length_error);
+    }
+
+    SECTION("Input fits") {
+      FixedCapacityVector<uint64_t, OtherCapacity> vector = {1UL, 2UL, 3UL};
+      FixedCapacityVector<uint64_t, Capacity> moved(std::move(vector));
+      REQUIRE(moved.size() == 3UL);
+      REQUIRE(moved.capacity() == Capacity);
+      REQUIRE(!moved.is_empty());
+      REQUIRE(moved.is_full());
+      REQUIRE(moved.front() == 1UL);
+      REQUIRE(moved.back() == 3UL);
+
+      REQUIRE(vector.is_empty());
+    }
   }
 }
